a68c8597ec6d73e546f61be7b8408b97f9c093d0
Toliman <skylake525@gmail.com>
2026-04-03 21:33:31 +0300

    sql pk

--- VocabularyTrainer.Database/Users.sql
+++ VocabularyTrainer.Database/Users.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[ID] INT NOT NULL PRIMARY KEY,
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Name] [nvarchar](64) NOT NULL UNIQUE,
 	[Info] [nvarchar](max) NULL,
 	[Login] [nvarchar](32) NOT NULL,
--- VocabularyTrainer.Database/Words.sql
+++ VocabularyTrainer.Database/Words.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Words]
 (
-	[ID] INT NOT NULL PRIMARY KEY,
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Value] [nvarchar](100) NOT NULL,
 	[Translation] [nvarchar](100) NOT NULL,
 )
