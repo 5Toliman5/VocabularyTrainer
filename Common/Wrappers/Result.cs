08ee250be2042e767bf33607f9c23b33c493c6cc
Toliman <skylake525@gmail.com>
2026-05-02 18:48:29 +0300

    changes

---
+++ Common/Wrappers/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace Common.Wrappers
+{
+    public interface IResult
+    {
+        bool Successful { get; }
+        string ErrorMessage { get; }
+    }
+
+    public abstract class ResultBase<T> : IResult
+        where T: ResultBase<T>, new()
+    {
+        public bool Successful { get; private init; }
+
+        public string ErrorMessage { get; private init; }
+
+        public static T Success()
+        {
+            return new T { Successful = true };
+        }
+
+        public static T Failure(string errorMessage)
+        {
+            return new T { Successful = false, ErrorMessage = errorMessage };
+        }
+    }
+
+    public class Result : ResultBase<Result>
+    {
+    }
+
+    public interface IResult<T> : IResult
+    {
+        T Value { get; }
+    }
+
+    public class Result<T> : ResultBase<Result<T>>, IResult<T>
+    {
+        public T Value { get; private set; }
+
+        public static Result<T> Success(T value)
+        {
+            var result = Success();
+            result.Value = value;
+            return result;
+        }
+    }
+}
